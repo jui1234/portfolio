c31f35dde82c6ebbf57a408af8ffc16fc1630c4e
jui <juiyezarkar1999@gmail.com>
2025-11-17 19:12:06 +0530

    navbar component

--- component/Navbar/Navbar.jsx
+++ component/Navbar/Navbar.jsx
@@ -1,5 +1,28 @@
+"use client";
+import navbarcss from './Navbar.css';
 export default function NavbarComponent() {
+
+    const toggleTheame=()=>{
+        document.documentElement.classList.toggle('dark-mode');
+    }
     return(
-        <>this is navbar component</>
+       <nav className="navbar-full">
+        <div className='navbar-logo'>
+            <div>Jui</div>
+        </div>
+        
+        <div className='navbar-info' >
+            <div className='navbar-other-info'>
+                <div>About</div>
+            <div>Project</div>
+            <div>Contact</div>
+            <div>resume button</div>
+            </div>
+            <div className='navbar-other-info'>
+              <button  style={{cursor:'pointer'}}onClick={toggleTheame}>M</button>
+            </div>
+            
+        </div>
+       </nav>
     )
 }
